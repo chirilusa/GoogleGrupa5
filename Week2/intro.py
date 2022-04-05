11843fc53571f1996e9bb2c821479d0a7d5f34d1
Andreea <chirilusa@gmail.com>
2022-04-05 21:15:31 +0200

    Create the files for Week2

---
+++ Week2/intro.py
@@ -0,0 +1,53 @@
+#numar_mere = 2
+#numar_pere = 3
+#mesaj = f"Ana are {numar_mere} mere." #cea mai utilizata
+#mesaj = "Ana are {0} mere si {1} pere.".format (numar_mere, numar_pere)
+#mesaj = "Ana are " + str(numar_mere) + " mere si " + str(numar_pere) + " pere."
+#print(mesaj)
+
+#lista = [1, 2, 4, 'Ana']
+# lista.append('2');
+#print(len(lista))
+#print(lista[2:5])
+
+#tuplu = (1, 3, -2, 'a')
+#tuplu = ('1')
+#print(type(tuplu))
+
+#dictionar = {"cheie1": 1, "cheie2": 2}
+#print(dictionar["cheie1"])
+#print(dictionar.get("cheie1"))
+#print(dictionar.get("cheie3", "3"))
+#dictionar["cheie3"] = "3"
+#dictionar.update = ({"cheie3": "3"})
+#print(dictionar.items())
+
+#my_set = {"item1", 1, 2, 3}
+#my_set1 = {"item2", 1, 2, 3}
+#print(my_set) #ordonare by default
+#print (my_set.union(my_set1))
+
+#var = 4;
+#mesaj = "conditii false"
+#if var < 3:
+#    mesaj = "Var este mai mic."
+#elif var > 3:
+#    mesaj = "Var este mai mare."
+#else:
+    #print("Conditii false!")
+#print (mesaj)
+
+#var1 = 1
+#while var1 < 3:
+#    print ("Set de instructiuni")
+#    var1 += 1
+#    break
+
+# word = "Ana are mere."
+# for  i, value in enumerate(word):
+   # print (i, value)
+#for i in range (2, 10, 2): #punem index de inceput daca vrem sa nu incepem de la inceput
+#    print(i)
+
+# ctrl / pentru comentat
+
