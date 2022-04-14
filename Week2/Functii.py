66e4f5709da0200e8ac84ce454100891e827fc96
Andreea <chirilusa@gmail.com>
2022-04-14 11:21:21 +0200

    Implement Functii.py.

---
+++ Week2/Functii.py
@@ -0,0 +1,21 @@
+# Exemple functii basic
+# def my_function (param):
+#     pass
+#
+# def sum (nr1: int, nr2: int = 2) -> int:
+#     """
+#     :param nr1: first number
+#     :param nr2: second number
+#     :return: sum of the numbers
+#     """
+#     return nr1 + nr2
+
+# def my_function (*param):
+#     print(type(param))
+#     return param
+#
+# print(my_function("str1", 'srt2'))
+
+#Rezolvare functii in echipa
+
+
