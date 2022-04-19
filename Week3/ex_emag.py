7d8d4494286da230e9f7e63d3e4dd4a28ee29972
Andreea <chirilusa@gmail.com>
2022-04-19 10:39:17 +0200

    Implement tema_week2.py.

---
+++ Week3/ex_emag.py
@@ -0,0 +1,19 @@
+import time
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+
+browser.get('https://www.emag.ro/#opensearch')
+get_element = browser.find_element(by = By.ID, value = 'searchboxTrigger')
+get_element.send_keys('telefon')
+get_element.submit()
+delay = 20 # seconds
+try:
+    myElem = WebDriverWait(browser, delay)
+    print ("Page is ready!")
+except TimeoutException:
+    print ("Loading took too much time!")
---
+++ Week3/namespaces.py
@@ -0,0 +1,26 @@
+# def my_function():
+#    msg = "Hello"
+#    print(msg)
+#    return None
+
+# my_function()
+# print(msg)
+# print(my_function())
+# print(msg)
+
+
+def my_function():
+    def my_second_function():
+        global msg
+        msg = "Hello"
+        return None
+
+    my_second_function()
+    msg = "Hello1"
+    print (f"Functia Principala {msg}")
+    return None
+
+
+my_function()
+print(msg)
+
