40a5529c707444cc3d39a6658bfa89da701df174
Andreea <chirilusa@gmail.com>
2022-04-19 12:09:47 +0200

    Implement ex_emag.py

--- Week3/ex_emag.py
+++ Week3/ex_emag.py
@@ -8,12 +8,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
 browser.get('https://www.emag.ro/#opensearch')
-get_element = browser.find_element(by = By.ID, value = 'searchboxTrigger')
+get_element = browser.find_element(by=By.ID, value='searchboxTrigger')
 get_element.send_keys('telefon')
 get_element.submit()
-delay = 20 # seconds
+delay = 20  # seconds
 try:
     myElem = WebDriverWait(browser, delay)
-    print ("Page is ready!")
+    print("Page is ready!")
 except TimeoutException:
-    print ("Loading took too much time!")
+    print("Loading took too much time!")
